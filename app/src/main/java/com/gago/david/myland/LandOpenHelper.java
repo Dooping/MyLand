c630d9470fdfc333a0888de735d0755b2e35a0f1
LAPTOP-IVKUUQD8\david <davidopg@gmail.com>
2017-01-27 19:32:47 +0000

    db creation

---
+++ app/src/main/java/com/gago/david/myland/LandOpenHelper.java
@@ -0,0 +1,53 @@
+package com.gago.david.myland;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by david on 27/01/2017.
+ */
+
+public class LandOpenHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String LAND_TABLE_NAME = "myland";
+
+    private Context context;
+
+    public LandOpenHelper(Context context) {
+        super(context, LAND_TABLE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        InputStream inputStream = context.getResources().openRawResource(R.raw.db);
+
+        String queries = "";
+        try {
+            queries = IOUtils.toString(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String query : queries.split(";")) {
+            db.execSQL(query);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    @Override
+    public void onConfigure(SQLiteDatabase db){
+        db.setForeignKeyConstraintsEnabled(true);
+    }
+}
